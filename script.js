3dc200d22ba4a0d8022260ca3e446255d0a76236
Olga Koval <ok@telemark-it.ru>
2019-12-31 10:48:48 +0300

    Updates

--- script.js
+++ script.js
@@ -13,7 +13,6 @@
   $('.medium-cards_slider').each(function() {
     $(this).slick({
       slidesToShow: 4,
-      slidesToScroll: 1,
       prevArrow: $(this).closest('.widget').find('.arrow-button_prev'),
       nextArrow: $(this).closest('.widget').find('.arrow-button_next'),
     });
