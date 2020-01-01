75f91eadbe52607b6e981862454f8c253aab6b52
Olga Koval <ok@telemark-it.ru>
2020-01-01 09:38:00 +0300

    fix

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-(function() {
+(function () {
   $('.big-cards_slider').each(function() {
     $(this).slick({
       slidesToShow: 1,
@@ -12,9 +12,25 @@
 
   $('.medium-cards_slider').each(function() {
     $(this).slick({
-      slidesToShow: 4,
+      slidesToShow: 2,
+      slidesToScroll: 1,
       prevArrow: $(this).closest('.widget').find('.arrow-button_prev'),
       nextArrow: $(this).closest('.widget').find('.arrow-button_next'),
+      mobileFirst: true,
+      responsive: [
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 640,
+          settings: {
+            slidesToShow: 4
+          }
+        },
+      ]
     });
   });
 })();
