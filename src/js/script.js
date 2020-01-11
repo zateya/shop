8f5ce2d2582cf6e54f2783677f27f59659747527
Olga Koval <ok@telemark-it.ru>
2020-01-11 20:05:24 +0300

    tablet and mobile layout change

--- src/js/script.js
+++ src/js/script.js
@@ -2,81 +2,98 @@
 
 (function () {
 
-  function setActiveClass (elem, activeClass) {
+  // Добавляет класс указанному элементу и убирает у соседних внутри переданного контейнера
+  function setActiveClass (container, elem, activeClass) {
     if (!elem.hasClass(activeClass)) {
-      $('.' + activeClass).removeClass(activeClass);
+      container.find('.' + activeClass).removeClass(activeClass);
     }
 
     elem.toggleClass(activeClass);
   };
 
+  // Подсвечивание нажатых кнопок поиска и пользовательского меню на телефонах
   $('.header__toggle').on('click', function (event) {
     event.preventDefault();
-    setActiveClass($(this), 'header__toggle_active');
+    setActiveClass($('.header__info'), $(this), 'header__toggle_active');
   });
 
+  // Нажатие кнопки открытия пользовательского меню на телефоне
   $('.header__user-toggle').on('click', function () {
     $('.header__search').fadeOut();
     $('.header__user-menu').fadeToggle();
   });
 
+  // Нажатие кнопки открытия поиска на телефоне
   $('.header__search-toggle').on('click', function () {
     $('.header__user-menu').fadeOut();
     $('.header__search').fadeToggle();
   });
 
+  // Сброс инлайновых стилей главного меню на десктопе
   function resetSiteNavStyle () {
     if(window.matchMedia('(min-width: 992px)').matches){
       $('.site-nav__list').removeAttr('style');
     }
-  }
+  };
 
+  // Сброс инлайновых стилей поиска и пользовательского меню на планшете
   function resetHeaderInfoStyle () {
     if(window.matchMedia('(min-width: 768px)').matches){
       $('.header__user-menu, .header__search').removeAttr('style');
       $('.header__toggle').removeClass('header__toggle_active');
     }
-  }
+  };
 
+  // Гамбургер открытия главного меню
   $('.site-nav__toggle').on('click', function (event) {
     event.preventDefault();
-    $('.aside-menu').slideUp();
+    $('.aside__menu').slideUp();
     $('.site-nav__list').slideToggle();
   });
 
+  // Гамбургер открытия меню каталога
   function onCatalogLinkClick () {
     event.preventDefault();
     $('.site-nav__list').slideUp();
-    $('.aside-menu').slideToggle();
+    $('.aside__menu').slideToggle();
   };
 
   $('.catalog-nav__link').addClass('bind').bind('click', onCatalogLinkClick);
 
-  function switchAsideMenu () {
+  // Добавление и снятиe событий открытия бокового меню (меню каталога)
+  function bindAsideMenu () {
     if(window.matchMedia('(max-width: 991px)').matches){
-      $('.aside-menu').removeClass('aside__menu').addClass('catalog-nav__menu');
-      $('.catalog-nav').append($('.aside-menu'));
       if (!$('.catalog-nav__link').hasClass('bind')) {
         $('.catalog-nav__link').addClass('bind').bind('click', onCatalogLinkClick);
       }
-      console.log(1);
     } else {
-      $('.aside-menu').removeClass('catalog-nav__menu').addClass('aside__menu').removeAttr('style');
-      $('.aside').prepend($('.aside-menu'));
+      $('.aside__menu').removeAttr('style');
       $('.catalog-nav__link').removeClass('bind').unbind('click', onCatalogLinkClick);
-      console.log(2);
     }
   };
 
+  // Ресайз окна браузера
   function onWindowResize () {
     resetSiteNavStyle();
     resetHeaderInfoStyle();
-    switchAsideMenu();
+    bindAsideMenu();
   };
 
   $(window).on('resize orientationchange', onWindowResize);
   onWindowResize();
 
+  // Слайдер витрины (большой баннер)
+  $('.showcase__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    speed: 1200,
+    autoplaySpeed: 7000,
+    fade: true,
+  });
+
+  // Слайдер карточек товаров в боковой колонке
   $('.big-cards_slider').each(function() {
     $(this).slick({
       slidesToShow: 1,
@@ -86,6 +103,7 @@
     });
   });
 
+  // Слайдер карточек товаров в контенте
   $('.medium-cards_slider').each(function() {
     $(this).slick({
       slidesToShow: 2,
@@ -110,6 +128,36 @@
     });
   });
 
+  // Фильтр карточек товаров
+  $('.filters__item').on('click', function (event) {
+    event.preventDefault();
+
+    var filterName = $(this).data('filter');
+    var activeClass = 'filters__item_active';
+
+    if (!$(this).hasClass(activeClass)) {
+      setActiveClass($(this).closest('.filters'), $(this), activeClass);
+
+      var slides = $(this).parents('.widget').find('[data-type]');
+
+      if (filterName === 'all') {
+        slides.show();
+
+        return false;
+      }
+
+      slides.each(function () {
+        if ($(this).data('type') !== filterName) {
+          $(this).hide();
+        } else {
+          $(this).show();
+        }
+      });
+    }
+
+  });
+
+  // Слайдер маленьких карточек товаров в контенте
   $('.small-cards-slider').each(function() {
     $(this).slick({
       slidesToShow: 1,
@@ -120,16 +168,7 @@
     });
   });
 
-  $('.showcase__slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    autoplay: true,
-    speed: 1200,
-    autoplaySpeed: 7000,
-    fade: true,
-  });
-
+  // Слайдер с логотипами брендов
   $('.brands-slider__items').slick({
     slidesToShow: 2,
     slidesToScroll: 1,
